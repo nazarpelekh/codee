29de85d381dd60b441301c91589f60f4d6837ea9
nazarpelekh <nazar_peleh@meta.ua>
2017-10-30 22:10:08 +0200

    codee

---
+++ footer.php
@@ -0,0 +1,12 @@
+</main>
+<footer>
+    <div class="wrap">
+        <div class="codee">
+            <span>[</span>codee<span>]</span> <?php echo date('Y') ?>
+        </div>
+        <?php echo soc(); ?>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+    <section class="top-block" style="background-image: url(<?php the_field('background-top') ?>);">
+        <div class="wrap">
+            <h1 class="codee">We <span>[</span>code<b class="element" data-words="<?php echo get_field('appears_text') ?>"></b><span>]</span></h1>
+        </div>
+    </section>
+    <section class="about">
+        <div class="wrap">
+            <h2>About Us</h2>
+
+        </div>
+    </section>
+<?php get_footer(); ?>
