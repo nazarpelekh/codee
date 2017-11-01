627d6fd5d7a318c786b517e8cac4be4e006f6842
nazarpelekh <nazar_peleh@meta.ua>
2017-11-01 09:19:46 +0200

    contact block done

--- front-page.php
+++ front-page.php
@@ -4,10 +4,10 @@
             <h1 class="codee">We <span>[</span>code<b class="element" data-words="<?php echo get_field('appears_text') ?>"></b><span>]</span></h1>
         </div>
     </section>
-    <section class="about">
+    <section class="contact-us">
         <div class="wrap">
-            <h2>About Us</h2>
-
+            <h2><?php _e("<!--:en-->Contact with us<!--:--><!--:ua-->Зв'яжіться з нами<!--:-->"); ?></h2>
+	        <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form"]') ?>
         </div>
     </section>
 <?php get_footer(); ?>
