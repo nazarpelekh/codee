1725e29187f8d814a87764cdf4cc4acdf2e0581e
nazarpelekh <nazar_peleh@meta.ua>
2017-11-15 17:48:52 +0200

    Update .gitignore

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <section class="top-block" style="background-image: url(<?php the_field('background-top') ?>);">
         <div class="wrap">
-            <h1 class="codee">We <span>[</span>code<b class="element" data-words="<?php echo get_field('appears_text') ?>"></b><span>]</span></h1>
+            <h1 class="codee"><span>[</span>code<b class="element" data-words="<?php echo get_field('appears_text') ?>"></b><span>]</span></h1>
         </div>
     </section>
     <section class="contact-us">
